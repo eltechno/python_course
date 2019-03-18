4f956e70499525110a8490aaabe26021649e6630
techno <techno.mayix@gmail.com>
2019-03-18 12:00:56 -0600

    the open file lab were modified with some documentation

--- LabSolution - read files.py
+++ LabSolution - read files.py
@@ -17,4 +17,17 @@ changes_files = open('charges_file.txt')
 for charges_info_string in changes_files:
     charges_info = charges_info_string.strip().split(',')
     print(charges_info)
-
+"""
+Mode 	Description
+r 	Opens a file for reading only. The file pointer is placed at the beginning of the file. This is the default mode.
+rb 	Opens a file for reading only in binary format. The file pointer is placed at the beginning of the file. This is the default mode.
+r+ 	Opens a file for both reading and writing. The file pointer will be at the beginning of the file.
+rb+ 	Opens a file for both reading and writing in binary format. The file pointer will be at the beginning of the file.
+w 	Opens a file for writing only. Overwrites the file if the file exists. If the file does not exist, creates a new file for writing.
+wb 	Opens a file for writing only in binary format. Overwrites the file if the file exists. If the file does not exist, creates a new file for writing.
+w+ 	Opens a file for both writing and reading. Overwrites the existing file if the file exists. If the file does not exist, creates a new file for reading and writing.
+a 	Opens a file for appending. The file pointer is at the end of the file if the file exists. That is, the file is in the append mode. If the file does not exist, it creates a new file for writing.
+ab 	Opens a file for appending in binary format. The file pointer is at the end of the file if the file exists. That is, the file is in the append mode. If the file does not exist, it creates a new file for writing.
+a+ 	Opens a file for both appending and reading. The file pointer is at the end of the file if the file exists. The file opens in the append mode. If the file does not exist, it creates a new file for reading and writing.
+ab+ 	Opens a file for both appending and reading in binary format. The file pointer is at the end of the file if the file exists. The file opens in the append mode. If the file does not exist, it creates a new file for reading and writing.
+"""
