35d3b1de83707f6b1a526711deeb8b4ff07e4d6c
techno <techno.mayix@gmail.com>
2019-03-21 14:38:10 -0600

    added new file

---
+++ decimal-example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Created by techno at 20/03/19
+
+#Feature:  #Enter feature name here
+# Enter feature description here
+
+#Scenario:  # Enter scenario name here
+# Enter steps here
+#import decimal
+from decimal import Decimal
+principal = Decimal('1000.00')
+
+print (principal)
