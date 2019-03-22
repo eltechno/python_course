266c2f06859c2f9141a1671cc1ee267a8d5a016b
techno <techno.mayix@gmail.com>
2019-03-22 14:52:52 -0600

    random number generator

--- random-number.py
+++ random-number.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 # Created by techno at 22/03/19
 
-Feature:  #Enter feature name here
+#Feature:  #Enter feature name here
 # Enter feature description here
 
-Scenario:  # Enter scenario name here
+#Scenario:  # Enter scenario name here
 # Enter steps here
+
+import random
+for roll in range(10):
+    print(random.randrange(1,  7), end=' ')
