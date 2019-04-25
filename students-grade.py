0671c65f607a2f12914f2e6e2bcefe89b9e00e2e
techno <techno.mayix@gmail.com>
2019-04-25 14:38:25 -0600

    some mods today

--- students-grade.py
+++ students-grade.py
@@ -2,8 +2,25 @@
 # -*- coding: utf-8 -*-
 # Created by techno at 25/04/19
 
-Feature:  #Enter feature name here
+#Feature:  #Enter feature name here
 # Enter feature description here
 
-Scenario:  # Enter scenario name here
+#Scenario:  # Enter scenario name here
 # Enter steps here
+
+""" using a dictionary  to represent an instructor's grade book"""
+grade_book ={
+    'Susan': [92, 85, 100],
+    'Eduardo': [83, 95, 79],
+    'Azizi': [91, 89, 82],
+    'Pantipa': [97, 91, 92]
+}
+all_grades_total=0
+all_grades_count=0
+
+for names, grades in grade_book.items():
+    total = sum(grades)
+    print(f'Averange for {names} is {total/len(grades):.2f}')
+    all_grades_total += total
+    all_grades_count += len(grades)
+print(f"Class's average is: {all_grades_total / all_grades_count:.2f}")
