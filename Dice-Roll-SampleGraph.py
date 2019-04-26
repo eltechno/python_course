611257b4aae7d629ea3dadb54782a1c7bddc344f
techno <techno.mayix@gmail.com>
2019-04-26 16:07:27 -0600

    dice roll sample graph

---
+++ Dice-Roll-SampleGraph.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Created by techno at 26/04/19
+
+Feature:  #Enter feature name here
+# Enter feature description here
+
+Scenario:  # Enter scenario name here
+# Enter steps here
