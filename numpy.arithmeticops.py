0a56fdb248c2c737b4b5c63692f61259e56aef8c
techno <techno.mayix@gmail.com>
2019-05-07 14:48:03 -0600

    some changes

---
+++ numpy.arithmeticops.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Created by techno at 3/05/19
+
+#Feature:  #Enter feature name here
+# Enter feature description here
+
+#Scenario:  # Enter scenario name here
+# Enter steps here
+
+
+
