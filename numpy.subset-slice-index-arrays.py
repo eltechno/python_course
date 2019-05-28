cf3a95e3f41f16c590a2b2f50f08170c1b15e605
techno <techno.mayix@gmail.com>
2019-05-28 10:55:58 -0600

    adding more examples with sile index subses arrays

--- numpy.subset-slice-index-arrays.py
+++ numpy.subset-slice-index-arrays.py
@@ -30,3 +30,36 @@ for i in range(4):
 
 #y = np.ones([4,4])
 #print(y)
+
+# Select items at index 0 and 1
+print(my_array[0:2])
+
+# Select items at row 0 and 1, column 1
+print(my_2d_array[0:2,1])
+
+# Select items at row 1
+# This is the same as saying `my_3d_array[1,:,:]
+print(my_3d_array[0,...])
+
+"""
+a[start:end] # items start through the end (but the end is not included!)
+a[start:]    # items start through the rest of the array
+a[:end]      # items from the beginning through the end (but the end is not included!)
+"""
+print()
+# Try out a simple example
+print(my_array[my_array<2])
+
+# Specify a condition
+bigger_than_3 = (my_3d_array >= 3)
+
+# Use the condition to index our 3d array
+print(my_3d_array[bigger_than_3])
+
+
+print(" first print")
+# Select elements at (1,0), (0,1), (1,2) and (0,0)
+print(my_2d_array[[1, 0, 1, 0],[0, 1, 2, 0]])
+print("second print")
+# Select a subset of the rows and columns
+print(my_2d_array[[1, 0, 1, 0]][:,[0,1,2,0]])
