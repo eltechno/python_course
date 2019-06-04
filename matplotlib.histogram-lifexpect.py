9ce2c88ac20554923e813c66ace494767369154a
techno <techno.mayix@gmail.com>
2019-06-03 21:44:13 -0600

    matplotlib create histogram

---
+++ matplotlib.histogram-lifexpect.py
@@ -0,0 +1,13 @@
+import pandas as pd
+globe =  pd.read_csv("gapminder.csv", skipinitialspace=True, usecols=['life_exp', 'gdp_cap'])
+life_exp = list(globe.life_exp)
+gdp_cap = list(globe.gdp_cap)
+
+
+import matplotlib
+matplotlib.use("TkAgg") #this is only for macOS
+from matplotlib import pyplot as plt
+
+
+plt.hist(life_exp)
+plt.show()
