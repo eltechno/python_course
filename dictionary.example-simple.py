67ef7e2a4f8c732b6b4f56b777f06cca023a01c4
techno <techno.mayix@gmail.com>
2019-06-07 12:24:31 -0600

    index simple example

---
+++ dictionary.example-simple.py
@@ -0,0 +1,14 @@
+
+pop = [30.55, 2.77, 39.21]
+countries = ["afganistan", "albania", "algeria"]
+
+print(countries.index("albania"))# show what index (position have albania in the array
+
+indice_alba = countries.index("albania") # assing the value of albania to a variable
+
+print(indice_alba) #print the value
+
+print(pop[indice_alba]) #print the value of albania index in the population array
+
+
+
