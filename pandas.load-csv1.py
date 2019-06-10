5916010531ed462e766f999e656c7c7b9ce8b631
techno <techno.mayix@gmail.com>
2019-06-10 12:03:51 -0600

    pandas first example how to load csv file

---
+++ pandas.load-csv1.py
@@ -0,0 +1,24 @@
+
+
+import pandas as pd
+cereal_df = pd.read_csv("cereal.csv", skiprows=0, na_values = ['no info', '.'])
+cereal_df2 = pd.read_csv("cereal2.csv")
+
+
+# skiprow used to skip the headers or any other line upper the header
+# na_values = ['no info', '.'] used Instead of parsing through each column
+# and replacing 'no info' and '.' with NaN values after the dataset is loaded,
+# you can use the na_values argument to account for those before it's loaded:
+
+#are they the same?
+print(pd.DataFrame.equals(cereal_df, cereal_df2))
+print(cereal_df.head(5))
+
+
+print(cereal_df['cups'].dtypes)
+print(cereal_df['name'].dtypes)
+print(cereal_df['fat'].dtypes)
+
+print(cereal_df['fat'][0])
+
+print(cereal_df['fat'][0] + cereal_df['fat'][1])
