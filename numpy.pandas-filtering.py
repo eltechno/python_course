374e2f174c65cbef2d9b86e1ea5f82b25c843f5a
techno <techno.mayix@gmail.com>
2019-06-20 15:59:09 -0600

    numpy and pandas combined to filter

---
+++ numpy.pandas-filtering.py
@@ -0,0 +1,25 @@
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Import numpy, you'll need this
+import numpy as np
+
+# Create medium: observations with cars_per_cap between 100 and 500
+
+cpc = cars['cars_per_cap']
+between = np.logical_and(cpc > 10, cpc < 80)
+medium = cars[between]
+
+
+# Print medium
+print(medium)
+
+
+#another filter
+
+print("cars_per_capita between 100 and 500")
+cpc = cars['cars_per_cap']
+between = np.logical_and(cpc > 100, cpc < 500)
+medium = cars[between]
+print(medium)
