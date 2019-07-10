7b2f589ccc12f10beed1177f8e725d911002becf
techno <techno.mayix@gmail.com>
2019-07-10 11:57:05 -0600

    lambda examples

--- Lamba.example1.py
+++ Lamba.example1.py
@@ -1,3 +1,55 @@
 add_bangs = (lambda a: a + "!!!")
 
 print(add_bangs("Hello"))
+
+
+# Define echo_word as a lambda function: echo_word
+echo_word = (lambda word1, echo: word1 * echo)
+
+# Call echo_word: result
+result = echo_word("hola",5)
+
+# Print result
+print(result)
+
+
+
+# Create a list of strings: spells
+spells = ["protego", "accio", "expecto patronum", "legilimens"]
+
+# Use map() to apply a lambda function over spells: shout_spells
+shout_spells_list = map(lambda spells: spells + "!!!", spells)
+
+# Convert shout_spells to a list: shout_spells_list
+shout_spells_list = (list(shout_spells_list))
+
+# Print the result
+print(shout_spells_list)
+
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'pippin', 'aragorn', 'boromir', 'legolas', 'gimli', 'gandalf']
+
+# Use filter() to apply a lambda function over fellowship: result
+result = filter(lambda member: len(member) > 6, fellowship)
+
+# Convert result to a list: result_list
+result_list = list(result)
+
+# Print result_list
+print(result_list)
+
+
+
+# Import reduce from functools
+
+from functools import reduce
+
+# Create a list of strings: stark
+stark = ['robb', 'sansa', 'arya', 'brandon', 'rickon']
+
+# Use reduce() to apply a lambda function over stark: result
+result = reduce(lambda item1, item2: item1 + item2, stark)
+
+# Print the result
+print(result)
