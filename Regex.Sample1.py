a7d56d746b470bbec4761be4f4760b4d3408c69d
techno <techno.mayix@gmail.com>
2019-07-29 10:20:52 -0600

    Regex sample 1

---
+++ Regex.Sample1.py
@@ -0,0 +1,14 @@
+#Regular Expression or regex
+#string containing a combination of normal characters and special metacharacters that describe
+#patters to find text or positions within a text
+
+
+#r'st\d\s\w\{3,10}'
+# Normal characters match themselves (st)
+# metacharacters \d (digit)
+# metacharacter \s (whitespace)
+# metacharacter \w (word character) or ideas( between 3 and 10)
+
+import re
+
+
