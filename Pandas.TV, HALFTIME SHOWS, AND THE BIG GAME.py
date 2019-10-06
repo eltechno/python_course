809c877e5724db5568122b83272031459dd6c8bb
techno <techno.mayix@gmail.com>
2019-10-06 17:44:38 -0600

    First Python Real Data Project

--- Pandas.TV, HALFTIME SHOWS, AND THE BIG GAME.py
+++ Pandas.TV, HALFTIME SHOWS, AND THE BIG GAME.py
@@ -62,3 +62,58 @@ games_tv = pd.merge(tv[tv['super_bowl'] > 1], super_bowls, on='super_bowl')
 
 # Create a scatter plot with a linear regression model fit
 sns.regplot(x='difference_pts', y='share_household', data=games_tv)
+
+
+# Create a figure with 3x1 subplot and activate the top subplot
+plt.subplot(3, 1, 1)
+plt.plot(tv.super_bowl, tv.avg_us_viewers, color="#648FFF")
+plt.title('Average Number of US Viewers')
+
+# Activate the middle subplot
+plt.subplot(3,1,2)
+plt.plot(tv.super_bowl, tv.rating_household, color="#DC267F")
+plt.title('Household Rating')
+
+# Activate the bottom subplot
+plt.subplot(3, 1, 3)
+plt.plot(tv.super_bowl, tv.ad_cost, color="#FFB000")
+plt.title('Ad Cost')
+plt.xlabel('SUPER BOWL')
+
+# Improve the spacing between subplots
+plt.tight_layout()
+
+# Display all halftime musicians for Super Bowls up to and including Super Bowl XXVII
+halftime_musicians[halftime_musicians.super_bowl <= 27]
+
+# Count halftime show appearances for each musician and sort them from most to least
+halftime_appearances = halftime_musicians.groupby('musician').count()['super_bowl'].reset_index()
+halftime_appearances = halftime_appearances.sort_values('super_bowl', ascending=False)
+
+# Display musicians with more than one halftime show appearance
+halftime_appearances[halftime_appearances['super_bowl'] > 1]
+
+
+# Filter out most marching bands
+no_bands = halftime_musicians[~halftime_musicians.musician.str.contains('Marching')]
+no_bands = no_bands[~no_bands.musician.str.contains('Spirit')]
+
+# Plot a histogram of number of songs per performance
+most_songs = int(max(no_bands['num_songs'].values))
+plt.hist(no_bands.num_songs.dropna(), bins= 5)
+plt.xlabel('Number of Songs Per Halftime Show Perfomance')
+plt.ylabel('Number of Musicians')
+plt.show()
+
+# Sort the non-band musicians by number of songs per appearance...
+no_bands = no_bands.sort_values('num_songs', ascending=False)
+# ...and display the top 15
+print(no_bands.head(15))
+
+# 2018-2019 conference champions
+patriots = 'New England Patriots'
+rams = 'Los Angeles Rams'
+
+# Who will win Super Bowl LIII?
+super_bowl_LIII_winner = rams
+print('The winner of Super Bowl LIII will be the', super_bowl_LIII_winner)
