c5a69a12458818a9ea6729d8732821e275eb1301
techno <techno.mayix@gmail.com>
2019-10-07 17:24:13 -0600

    python importing data from files

---
+++ Files.read-plain-text.py
@@ -0,0 +1,20 @@
+# Open a file: file
+file = open("moby_dick.txt", "r")
+
+# Print it
+print(file.read())
+
+# Check whether file is closed
+print(file.closed)
+
+# Close file
+file.close()
+
+# Check whether file is closed
+print(file.closed)
+
+# Read & print the first 3 lines
+with open('moby_dick.txt') as file:
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
