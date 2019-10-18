33da7c9d063b731be960f9a7505242f96d1b9bf3
techno <techno.mayix@gmail.com>
2019-10-17 21:11:09 -0600

    importing data from hdf5 and matlab files

--- File.HDF5-load.py
+++ File.HDF5-load.py
@@ -2,6 +2,8 @@
 #hierarchical Data Format Version 5
 
 import h5py
+import numpy as np
+from matplotlib import pyplot as plt
 filename = 'L-L1_LOSC_4_V1-1126259446-32.hdf5'
 data = h5py.File(filename, 'r') # r is to read
 print(type(data))
@@ -15,3 +17,27 @@ for key in data['meta'].keys():
     print(key)
 
 print(data['meta']['Description'].value, data['meta']['Detector'].value)
+
+
+#checking another group
+# Get the HDF5 group: group
+group = data['strain']
+
+# Check out keys of group
+for key in group.keys():
+    print(key)
+
+# Set variable equal to time series data: strain
+strain = data['strain']['Strain'].value
+
+# Set number of time points to sample: num_samples
+num_samples = 10000
+
+# Set time vector
+time = np.arange(0, 1, 1/num_samples)
+
+# Plot data
+plt.plot(time, strain[:num_samples])
+plt.xlabel('GPS Time (s)')
+plt.ylabel('strain')
+plt.show()
