bfffb8dc646beea0e1a440255bb353c341c5ccd5
techno <techno.mayix@gmail.com>
2019-10-22 22:41:24 -0600

    update adding northwind sqlite

--- File.DB-query-inner-join.py
+++ File.DB-query-inner-join.py
@@ -1,13 +1,12 @@
-#from sqlalchemy import create_engine
-#import pandas as pd
-
-#engine = create_engine("sqlite:///Northwind_large.sqlite")
-#df = pd.read_sql_query("Select OrderID, CustomerID from Orders INNER JOIN Customer on Orders.CustomerID = Customers.CustomerID", engine)
-#print(df.head())
-
-
-import pandas as pd
 from sqlalchemy import create_engine
+import pandas as pd
+
+engine = create_engine("sqlite:///Northwind.sqlite")
+df = pd.read_sql_query("Select OrderID, CustomerID from Orders INNER JOIN Customer on Orders.CustomerID = Customers.CustomerID", engine)
+print(df.head())
+
+
+
 engine = create_engine('sqlite:///Chinook.sqlite')
 
 # Open engine in context manager
