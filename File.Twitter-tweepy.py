238df50617e08254f70a06127ea0ea65b9be47aa
techno <techno.mayix@gmail.com>
2019-10-24 21:48:26 -0600

    tweepy twitter

---
+++ File.Twitter-tweepy.py
@@ -0,0 +1,32 @@
+import tweepy, json
+
+access_token = "8680022-"
+access_token_secret=""
+consumer_key=""
+consumer_secret=""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token=(access_token, access_token_secret)
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api=None):
+        super(MyStreamListener, self).__init__()
+        self.num_tweets = 0
+        self.file = open("tweets.txt", "w")
+    def on_status(self, status):
+        tweet = status._json
+        self.file.write(json.dump(tweet) + '\\n')
+        tweet_list.append(status)
+        self.num_tweets += 1
+        if self.num_tweets < 100:
+            return True
+        else:
+            return False
+        self.file.close()
+
+# create streaming object and authenticate
+l = MyStreamListener()
+stream = tweepy.Stream(auth,l)
+
+#this line filter twitter streams to capture data by eywords
+stream.filter(track=['ctm', 'r  usa'])
