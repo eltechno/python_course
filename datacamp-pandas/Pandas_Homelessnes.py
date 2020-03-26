7ab3a77bd0b541301dc8db9d683058c90ad68bc2
techno <techno.mayix@gmail.com>
2020-03-25 19:06:06 -0600

    pandas examples

--- datacamp-pandas/Pandas_Homelessnes.py
+++ datacamp-pandas/Pandas_Homelessnes.py
@@ -51,3 +51,5 @@ print(mountain_reg)
 fam_lt_1k_pac = homelessness[(homelessness["family_members"] < 1000) & (homelessness["region"] == "Pacific")]
 # See the result
 print(fam_lt_1k_pac)
+
+#adding updates
