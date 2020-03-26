7888de6279c8e01d9cdb6d5adc117d525408e800
techno <techno.mayix@gmail.com>
2020-03-26 17:51:27 -0600

    adding some updates

--- datacamp-pandas/Pandas_Homelessnes.py
+++ datacamp-pandas/Pandas_Homelessnes.py
@@ -53,3 +53,40 @@ fam_lt_1k_pac = homelessness[(homelessness["family_members"] < 1000) & (homeless
 print(fam_lt_1k_pac)
 
 #adding updates
+
+# Subset for rows in South Atlantic or South Atlantic regions
+regions = ["South Atlantic", "Mid-Atlantic"]
+condition = homelessness["region"].isin(regions)
+south_mid_atlantic = homelessness[condition]
+# See the result
+print(south_mid_atlantic)
+
+
+# The Mojave Desert states
+canu = ["California", "Arizona", "Nevada", "Utah"]
+# Filter for rows in the Mojave Desert states
+contidion = homelessness["state"].isin(canu)
+mojave_homelessness = homelessness [contidion]
+# See the result
+print(mojave_homelessness)
+
+
+# Add total col as sum of individuals and family_members
+homelessness["total"] = homelessness["individuals"] + homelessness["family_members"]
+# Add p_individuals col as proportion of individuals
+homelessness["p_individuals"] = homelessness["individuals"]/ homelessness["total"]
+#to get proportion always divide into the total
+# See the result
+print(homelessness)
+
+
+# Create indiv_per_10k col as homeless individuals per 10k state pop
+homelessness["indiv_per_10k"] = 10000 * homelessness["individuals"] / homelessness["state_pop"]
+# Subset rows for indiv_per_10k greater than 20
+high_homelessness = homelessness[homelessness["indiv_per_10k"] > 20]
+# Sort high_homelessness by descending indiv_per_10k
+high_homelessness_srt = high_homelessness.sort_values("indiv_per_10k", ascending=[False])
+# From high_homelessness_srt, select the state and indiv_per_10k cols
+result = high_homelessness_srt[["state","indiv_per_10k"]]
+# See the result
+print(result)
