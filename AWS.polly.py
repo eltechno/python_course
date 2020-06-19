f74abac7df3f9f41864afd06479389260c29ea3a
techno <techno.mayix@gmail.com>
2020-06-19 10:19:14 -0600

    Amazon Web Services Polly

--- AWS.polly.py
+++ AWS.polly.py
@@ -1,13 +1,14 @@
 import boto3
 
 polly_client = boto3.Session(
-                aws_access_key_id="AKIA4A7DY6PRJDZS4PQW",
-    aws_secret_access_key="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",
+                aws_access_key_id="",
+    aws_secret_access_key="",
     region_name='us-west-2').client('polly')
 
-response = polly_client.synthesize_speech(VoiceId='Joanna',
+#response = polly_client.synthesize_speech(VoiceId='Joanna',
+response = polly_client.synthesize_speech(VoiceId='Miguel',
                 OutputFormat='mp3',
-                Text = 'This is a sample text to be synthesized.')
+                Text = 'Gracias por comunicarte con nosotros, en un momento te atenderemos, si quieres comunicarte, con ventas, marca uno, contabilidad, marca dos')
 
 file = open('speech.mp3', 'wb')
 file.write(response['AudioStream'].read())
